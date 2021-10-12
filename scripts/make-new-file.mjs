f3eed2181d675f23b40010e1fd3afe5b995b48b2
jacob whitford-bender <jacob@xmentium.com>
2021-10-11 20:18:58 -0600

    Auto saved by Logseq

--- scripts/make-new-file.mjs
+++ scripts/make-new-file.mjs
@@ -92,6 +92,42 @@ const main = path => ({title, ...meta}) => content =>
 	writeFile (path) (title) (fromPunchKopipe ({title, ...meta}) (content))
 
 const punches = [
+	`
+
+Creamy Martini
+White Lyan | London, UK
+
+    Share story:
+    Share
+    Tweet
+    14Save
+    Email
+
+At the pioneering cocktail bar White Lyan (which closed in April 2017), they used lactic acid—perhaps best known as the agent that gives yogurt or kefir its tang—when looking to enhance savory flavors and round off the edges of a drink; as a milder acid, it actually helps add a fuller, creamy mouthfeel to cocktails, as has a slightly higher pH than citric or malic acid. It’s used in the recipe for the Creamy Martini to, well, give it creaminess.
+
+    Print
+    Save
+
+Ingredients
+
+Serving: 1
+
+    2 ounces gin
+    1/4 ounce dry vermouth
+    2 drops 5% lactic acid solution, (see Editor's Note)
+
+Directions
+
+    Add all ingredients to a mixing glass, add ice and stir.
+    Strain into a cocktail glass.
+
+Editor's Note
+
+Lactic Acid 5% Solution:
+Combine 5 grams of lactic acid (usually comes as a 70% solution) with 100 grams of water. Stir to blend, then bottle.
+	`,
+	`
+	`,
 ]
 
 const isString = pipe ([type, ({name}) => name === 'String'])
