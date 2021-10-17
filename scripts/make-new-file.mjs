988dafbbc1643d1a28db1023a268de5a401aa5bd
jacob whitford-bender <jacob@xmentium.com>
2021-10-17 15:48:00 -0600

    Auto saved by Logseq

--- scripts/make-new-file.mjs
+++ scripts/make-new-file.mjs
@@ -166,6 +166,46 @@ Vanilla Syrup
 Combine 250 grams caster sugar, 250 grams water and 1 vanilla bean (both pod and scraped seeds) in a saucepot. Bring to a boil, then reduce to low heat for 20 minutes. Allow to cool, then strain out solids and refrigerate in a sealable container.
 
 	`,
+	`
+
+Perfect Manhattan
+The classic mixed with sweet and dry vermouth.
+photo: Daniel Krieger
+
+    Share story:
+    Share
+    Tweet
+    12Save
+    Email
+
+    Cocktail Strong
+
+One of the heavyweights in the cocktail world, the Manhattan is something of a twist on the Old Fashioned, most likely spurred by sweet vermouth’s arrival and ensuing popularity in the United States during the latter part of the 19th century. The too-good-to-be-true story surrounding this drink’s origins—that it was invented at the Manhattan Club for an event with Winston Churchill’s mother—is just that: a tall tale. There’s still some debate among cocktail historians, but current theories for the original recipe favor either the Manhattan Club (sans Churchill), or a waiter named “Black,” who worked in lower Manhattan in the 1870s. There are other cocktails named for each of New York’s boroughs, but none as enduring or popular as the Manhattan. The “perfect” modifier indicates a combination of dry and sweet vermouth in equal parts as opposed to the original Manhattan’s full measure of sweet vermouth.
+
+    Print
+    Save
+
+Ingredients
+
+Serving: 1
+
+    2 ounces rye or bourbon
+    1/2 ounce sweet vermouth
+    1/2 ounce dry vermouth
+    2 dashes Angostura bitters
+
+Garnish: brandied cherry (preferably Luxardo) or a lemon twist
+Directions
+
+    Add all ingredients to a mixing glass.
+    Add ice and stir well.
+    Strain into a chilled coupe or cocktail glass.
+    Garnish with a brandied cherry or a lemon twist.
+
+Editor's Note
+
+We believe the Manhattan behaves best with rye (specifically Rittenhouse), even though bourbon has become its de facto base spirit over the years. Carpano Antica is the preferred sweet vermouth, but unfortunately it can be elusive, so in a pinch Dolin, or even Martini & Rossi, will work. If you’re a bourbon fan, Evan Williams is versatile and delivers the sweet simplicity most look for in the spirit. Lastly, Luxardo cherries are a luxury, but if you can find them, plop one in. Otherwise, go for a lemon twist.
+	`,
 ]
 
 const isString = pipe ([type, ({name}) => name === 'String'])
