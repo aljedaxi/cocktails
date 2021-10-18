968dbf2e3e85642137b49c1e2347e4f1fdf2d8cb
jacob whitford-bender <jacob@xmentium.com>
2021-10-17 22:35:57 -0600

    Auto saved by Logseq

--- scripts/logseq.mjs
+++ scripts/logseq.mjs
@@ -12,3 +12,31 @@ ${ingredients ?? '  -'}
 ${instructions ?? '  -'}
 `
 )
+
+const section = s => `- ${s}`
+const lItem = s => `  - ${s}`
+export const markDown = ({path, tags}) => pipe([
+	({name, author, source, ingredients, instructions, description}) => ({
+		fileName: `${path}${name.replace(/\s/g, '-')}.md`,
+		metadata: {
+			title: name, 
+			author, 
+			source: fromMaybe ('') (source),
+			tags: tags.join(', ')
+		},
+		text: joinWith ('\n') ([
+			section ('Description'),
+			description,
+			section ('Ingredients'),
+			...map (lItem) (ingredients),
+			section ('Steps'),
+			...map (lItem) (instructions),
+		])
+	}),
+	({metadata, text, fileName}) => Pair (fileName) ([
+		`---`,
+		Object.entries(metadata).map(([k,v]) => `${k}: ${v}`).join('\n'),
+		`---`,
+		text,
+	].join('\n')),
+])
