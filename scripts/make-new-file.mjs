594ec282bd07e8c9c5651ba15c2aae345b091d19
jacob whitford-bender <jacob@xmentium.com>
2021-10-17 23:09:57 -0600

    Auto saved by Logseq

--- scripts/make-new-file.mjs
+++ scripts/make-new-file.mjs
@@ -120,39 +120,6 @@ const processString = pipe ([
 ])
 
 const punches = [
-	`
-Toronto
-Adapted from The Fine Art of Mixing Drinks by David A. Embury | 1948
-photo: Eric Medsker
-
-    Share story:
-    Share
-    Tweet
-    1Save
-    Email 
-
-    Classics Strong 
-
-Similar in construction to the earlier “Fernet Cocktail” appearing in Robert Vermier’s 1922 book, Cocktails: How to Mix Them, the Toronto is a spirit-forward Fernet-laced drink pulled from the pages of David A. Embury’s 1948 title The Fine Art of Mixing Drinks.
-
-    Print
-    Save
-
-Ingredients
-
-Serving: 1
-
-    2 ounces rye
-    1/4 ounce Fernet-Branca
-    1/4 ounce demerara syrup
-    2 dashes Angostura bitters
-
-Garnish: orange twist
-Directions
-
-    Combine all ingredients in a mixing glass with ice and stir until chilled.
-    Strain into a chilled coupe or cocktail glass.
-    Garnish with an orange twist.`,
 ]
 
 await Promise.all (
