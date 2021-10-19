20c52e7b0687c44be60466db7555c20b4930b5b3
jacob whitford-bender <jacob@xmentium.com>
2021-10-18 19:09:13 -0600

    Auto saved by Logseq

--- scripts/punch.mjs
+++ scripts/punch.mjs
@@ -120,6 +120,41 @@ const processString = pipe ([
 ])
 
 const punches = [
+	`
+Nuked Negroni
+Adapted from Good Things to Drink with Mr Lyan and Friends
+
+    Share story:
+    Share
+    Tweet
+    5Save
+    Email
+
+    Cocktail Bitter Strong
+
+This is London modernist bartender Ryan Chetiyawardana’s favorite recipe from his first book and, he says, “the one people seem to make and tag me in the most.” This batched version of the classic cocktail takes an unexpected turn by way of a quick infusion in the microwave (yes, the microwave).
+
+    Print
+    Save
+
+Ingredients
+
+Servings
+
+    1 1/2 cups gin
+    1 1/2 cups Campari
+    1 1/2 cups sweet vermouth
+    6 blackberries
+    1 grapefruit peel
+    1 sprig rosemary
+
+Directions
+
+    Add all ingredients to a closable microwave container.
+    Add to the microwave and blast at 600 watts for 3 minutes.
+    Allow to cool and filter.
+    Serve over ice in a rocks glass.
+	`,
 ]
 
 await Promise.all (
